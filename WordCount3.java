1b82dfb724f613e98294d15164efd81c208ff227
장현지 <mybye1001@naver.com>
2019-11-05 11:11:39 +0900

    wordcountCode

---
+++ WordCount3.java
@@ -0,0 +1,70 @@
+package mju.hadoop.wordcount;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class WordCount3 {
+    public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(RemoveSpecialCharac2(line));
+            	while (tokenizer.hasMoreTokens()) {
+            		word.set(tokenizer.nextToken());
+            		context.write(word, one);
+            	}
+        }
+    }
+    public static String RemoveSpecialCharac2(String str) {
+    	String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z_\\s]";
+    	str = str.replaceAll(match, "");
+    	return str;
+    }
+    
+
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable value = new IntWritable(0);
+        @Override
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable value : values)
+                sum += value.get();
+            value.set(sum);
+            context.write(key, value);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = new Job(conf, "wordcount3");
+        job.setJarByClass(WordCount3.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        job.setNumReduceTasks(1);
+
+        FileInputFormat .setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        boolean success = job.waitForCompletion(true);
+        System.out.println(success);
+    }
+}
